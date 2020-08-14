602420e21bea75219c75f5aca2ec02fb906cb483
Trevis McGill <trevismcgill25@gmail.com>
2020-08-14 21:19:17 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,13 +8,13 @@ def self.scrape_index_page(index_url)
 
 doc = Nokogiri::HTML(open(index_url))
 doc.css(".student-card").each do |student|
+  binding.pry
   student_info = {}
   student_info[:student_links] = student.css("a")["href"]#doc.css(".student-card a").collect {|link| link["href"]}
   student_info[:student_names] = student.css("h4").text#doc.css('h4').collect {|name| name.text}
   student_info[:student_locations] = student.css("p").text#doc.css('p').collect {|location| location.text}
   @@students.push(student)
-# binding.pry
-@@students
+  @@students
   end
 end
 
