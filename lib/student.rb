f4d34f5bfaab541f13a6e1a7d55d8eb090115d23
Trevis McGill <trevismcgill25@gmail.com>
2020-08-16 19:10:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    binding.pry
+    # binding.pry
     attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
